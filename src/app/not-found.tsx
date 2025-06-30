788e947a3aff5e2330c1fbb30da1e63e8f804dbb
shawonahme <shawonahmed55660@gmail.com>
2025-06-30 06:05:11 +0600

    error page setup

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='min-h-screen dark:bg-black text-red-500 text-xl font-extrabold flex justify-center items-center'>
+                 404 |  PAGE NOT FOUND
+            
+        </div>
+    );
+};
+
+export default NotFound;
